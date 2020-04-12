8ba03ca56054b124fd1d5006979e5e286ba649cf
effraim <32572199+effraim@users.noreply.github.com>
2020-04-12 19:02:37 +0300

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -35,10 +35,7 @@ def get_char(text,pos):
 	
 	c = text[pos]
 	
-	# (Γ) Προαιρετικά, μπορείτε να ομαδοποιήσετε τους
-	# χαρακτήρες εισόδου εδώ.
-	# Για λεπτομέρειες δείτε στο:
-	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id11
+	if c >= "0" and c <= "9": return "DIGIT"
 	
 	return c
 	
